2720f09a0b7496ad9d4ecc1e9d8c3050c5f67e18
jkwaky <jkwak@utexas.edu>
2018-08-13 16:10:31 -0500

    push project9

---
+++ lab9/printadc.c
@@ -0,0 +1,64 @@
+// main.c
+// Runs on LM4F120/TM4C123
+// UART runs at 115,200 baud rate 
+// Daniel Valvano
+// May 3, 2015
+
+/* This example accompanies the books
+  "Embedded Systems: Introduction to ARM Cortex M Microcontrollers",
+  ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2015
+"Embedded Systems: Real Time Interfacing to ARM Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
+ 
+ Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+
+#include <stdint.h> // C99 variable types
+#include "ADCSWTrigger.h"
+#include "uart.h"
+#include "PLL.h"
+#include "../inc/tm4c123gh6pm.h"
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+long StartCritical (void);    // previous I bit, disable interrupts
+void EndCritical(long sr);    // restore I bit to previous value
+void WaitForInterrupt(void);  // low power mode
+
+extern uint32_t ADCvalue[100];
+extern uint8_t indexholder;
+
+int main(void){ 
+	//int32_t data;
+  PLL_Init(Bus80MHz);   // 80 MHz
+  UART_Init();              // initialize UART device
+  SYSCTL_RCGCGPIO_R |= 0x00000020;         // activate port F
+  ADC0_InitTimer0ATriggerSeq3(0, 80000); // ADC channel 0, 10 Hz sampling
+  GPIO_PORTF_DIR_R |= 0x04;                // make PF2 out (built-in LED)
+  GPIO_PORTF_AFSEL_R &= ~0x04;             // disable alt funct on PF2
+  GPIO_PORTF_DEN_R |= 0x04;                // enable digital I/O on PF2
+                                           // configure PF2 as GPIO
+  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFF0FF)+0x00000000;
+  GPIO_PORTF_AMSEL_R = 0;                  // disable analog functionality on PF
+  GPIO_PORTF_DATA_R &= ~0x04;              // turn off LED
+  EnableInterrupts();
+  while(indexholder<100){}
+		int uartindex = 0;
+		while(uartindex<100){
+    //data = ADC0_InSeq3();
+    UART_OutString("\n\r");
+			//");
+    UART_OutUDec(ADCvalue[uartindex]);
+		uartindex++;
+  }
+}
